44f65c0ef2481074a38531ef13313211a60915a5
Christopher Brown <christopherbrown.contact@gmail.com>
2021-09-20 14:44:54 -0700

    added gamePlay(), short if/else

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,34 @@
 
-
+// batter object hold key value currentSwing null to be evaluated throughout game play
 let batter = {
     currentSwing: null
 };
-
+// starter pack basic array for hit or strike to be randomized throughout game play
 let swingArray = ["hit", "strike"];
 
-
+// function to run get random swing 
 function getSwing() {
   const randomSwing = Math.floor(Math.random() * swingArray.length); 
   batter.currentSwing = swingArray[randomSwing]; 
 }
 
-console.log("get swing func work: ",getSwing(), "current swing: ", batter.currentSwing);
+// testing zone 
+// console.log(getSwing(), batter.currentSwing);
+
+function gamePlay(){
+
+    getSwing();
+
+    if(batter.currentSwing === "hit"){
+        console.log("It's a hit!");
+
+
+    }else{
+        console.log("Strike!");
+    }
+
+}
+
+gamePlay();
+
 
