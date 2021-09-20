cc00b5d7aff5ef4f48313a204818912534f95822
Christopher Brown <christopherbrown.contact@gmail.com>
2021-09-20 15:59:47 -0700

    lets for hits and strikes totals, increment hits and strikes function added to flow of conditionals

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,27 @@
 
-// batter object hold key value currentSwing null to be evaluated throughout game play
+let hitsTotal = 0;
+let strikesTotal = 0;
+
+function addHits(){
+    hitsTotal = hitsTotal + 1;
+    console.log(hitsTotal);
+}
+
+function addStrikes(){
+    strikesTotal = strikesTotal + 1;
+    console.log(strikesTotal);
+}
+
+
+// batter object hold key-value for currentSwing null to be evaluated throughout game play
 let batter = {
     currentSwing: null
 };
-// starter pack basic array for hit or strike to be randomized throughout game play
+
+
+// array for hit or strike values to be randomized throughout game play
 let swingArray = ["hit", "strike"];
+
 
 // function to run get random swing 
 function getSwing() {
@@ -12,23 +29,30 @@ function getSwing() {
   batter.currentSwing = swingArray[randomSwing]; 
 }
 
-// testing zone 
-// console.log(getSwing(), batter.currentSwing);
 
+// RUNS GAMEPLAY 
 function gamePlay(){
 
     getSwing();
 
     if(batter.currentSwing === "hit"){
+        addHits();
         console.log("It's a hit!");
-
+        console.log(hitsTotal);
 
     }else{
+        // strikesTotal = strikesTotal + 1; 
+        addStrikes();
         console.log("Strike!");
+        console.log(strikesTotal);
+        
     }
 
 }
 
 gamePlay();
+
+
+
 
 
