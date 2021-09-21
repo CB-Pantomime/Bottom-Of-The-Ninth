e51cc6208f5ed4bb3ea096dee7bbbd517b3bd7cd
Christopher Brown <christopherbrown.contact@gmail.com>
2021-09-20 19:41:29 -0700

    fouls nested conditionals working. tested loaded variables, each incremented to next move is a win for either team.

--- scripts/app.js
+++ scripts/app.js
@@ -1,33 +1,33 @@
 // all those variables ... 
-let hitsTotal = 0;
-let strikesTotal = 0;
-let foulsTotal = 0;
-let ballsTotal = 0;
+let hitsTotal = 3;
+let strikesTotal = 8;
+let foulsTotal = 2;
+let ballsTotal = 3;
 
 
 // TESTED OK
 function addHits(){
     hitsTotal = hitsTotal + 1;
-    console.log(hitsTotal);
+    // console.log("hits total: ", hitsTotal);
 }
 // TESTED OK
 function addStrikes(){
     strikesTotal = strikesTotal + 1;
-    console.log(strikesTotal);
+    console.log("strikes total: ", strikesTotal);
 }
 
 function addFouls(){
     foulsTotal = foulsTotal + 1;
-    console.log(foulsTotal);
+    // console.log("fouls total: ", foulsTotal);
 } 
 // TESTED OK
 function addBalls(){
     ballsTotal = ballsTotal + 1;
-    console.log(ballsTotal);
+    // console.log("balls total: ", ballsTotal);
 }
 // TESTED OK
 function endOfGameParade(){
-    console.log("HOORAY LOL");
+    console.log("~STADIUM GOES BERSERK HOORAY LOL~");
 }
 
 
@@ -68,7 +68,6 @@ function gamePlay(){
             }
 
 
-
     }else if(batter.currentSwing ==="strike"){
          
                     // TESTED OK
@@ -83,22 +82,23 @@ function gamePlay(){
             }
         
         
-    }
-    
-    // else if(batter.currentSwing === "foul"){
+    }else if(batter.currentSwing === "foul"){
 
+                    // // TESTED OK
+            addFouls();
+            if(foulsTotal <= 2){
+                addStrikes();
+                console.log("Foul! Another strike!");
+                console.log("fouls total: ", foulsTotal);
+            }else{
+                console.log("WOW U KEEP FOULING OUT");
+                console.log("fouls total: ", foulsTotal);
+            }
+       
 
-    //         addFouls();
-    //         if(foulsTotal === ){
-                
-    //         }
-    //     // console.log("Foul!");
-    //     // console.log("fouls total: ", foulsTotal);
-    // }
-    
-    else if(batter.currentSwing === "ball"){
+    }else if(batter.currentSwing === "ball"){
 
-                    // TESTED OK
+                    // // TESTED OK
         addBalls();
         if(ballsTotal === 4){
             addHits();
@@ -116,11 +116,12 @@ function gamePlay(){
         }
         
     }
-
+    
 };
 
 
 gamePlay();
+
 
 // for(let i = 0; i < 10; i++){
 //     gamePlay();
