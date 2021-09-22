808a551bfa92b13196a2136ebbd3310fc8013a1a
Christopher Brown <christopherbrown.contact@gmail.com>
2021-09-22 16:20:59 -0700

    The swingButton now only display until game starts, and is not displayed at end of game. vars refactored/debugged. 'batter out' and 'new batter up' now included in and displays during relevant gameplay. basic layout html/css working, with active buttons and gameplay display.

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,26 @@
 
-// all those variables ... 
+// ***ALL THOSE VARIABLES*** 
 let hitsTotal = 0;
 let strikesTotal = 0;
 let foulsTotal = 0;
 let ballsTotal = 0;
 
-// To display initial wind up and pitch, to display swingButton
+
+// ***FUNCTIONS***
+// TESTED OK
 function startGame(){
+  document.getElementById("swingButton").style.display = "block";
   document.getElementById("swingButton").innerHTML = "SWING!";
   setTimeout(showWindUp, 1000), 
   setTimeout(showPitch, 3200)
-//   console.log("start game button works");
+  // console.log("start game button works");
 }
-
 
 // TESTED OK
 function endOfGameParade(winString){
   let parade = document.getElementById("currentAction");
   parade.innerHTML = winString;
-  // console.log("~STADIUM GOES BERSERK HOORAY LOL~");
 }
-
 
 // TESTED OK
 // batter object hold key-value for currentSwing null to be evaluated throughout game play
@@ -30,10 +30,10 @@ let batter = {
 
 // TESTED OK
 // array for hit or strike values to be randomized throughout game play
-let swingArray = ["hit", "hit", "strike", "foul", "ball"];
+let swingArray = ["hit", "strike", "foul", "ball"];
 
 // TESTED OK
-// function to run get random swing 
+// function to run get random swing result from swingArray 
 function getSwing() {
   const randomSwing = Math.floor(Math.random() * swingArray.length); 
   batter.currentSwing = swingArray[randomSwing]; 
@@ -49,7 +49,7 @@ function showWindUp(){
 function showPitch(){  
   document.getElementById("currentAction").innerHTML = "Here comes the pitch!";
 }
-// To group wind up and pitch to be called in conditionals as relevant
+// To group wind up and pitch to be called in conditionals as relevant w/ increased timeout 
 function windAndPitchScript(){
   setTimeout(showWindUp, 5000), 
   setTimeout(showPitch, 7200)
@@ -66,6 +66,20 @@ function gameOver(){
   document.getElementById("currentAction").innerHTML = "GAME OVER THX 4 PLAYING"
 }
 
+function turnOffSwing(){
+  let turnOffSwing = document.getElementById("swingButton");
+  turnOffSwing.style.display = "none"; 
+}
+
+// freakin' just use this lol fisher-yates is too crazy 4 me rn
+// let battersArray = ["CB", "Kev", "Maud", "Jill"];
+
+// let sortedBatters = [];
+
+// sortedBatters = battersArray.sort( (a,b) => 0.5 - Math.random() );
+// console.log(sortedBatters);
+
+
 
 // TESTED OK
 // CLICK SWING RUNS GAMEPLAY 
@@ -81,12 +95,13 @@ function gamePlay(){
                 // setTimeout(currentAction(`Batting team wins!`), 3000);
                 // console.log("Batting team wins");
                 // console.log("hits total: ", hitsTotal);  
-                setTimeout( endOfGameParade(`Batting team wins!`), 3000  ); 
-                return setTimeout( gameOver(), 5000 );
+                endOfGameParade(`A run is batted in! Batting team wins! GAME OVER THX 4 PLAYING`);            
+                turnOffSwing();
             }else{ 
-            // add update current action to "showPitch" ID which needs a name change to encompass maybe "currentAction"
+           
+            // To display current game result AND new batter up 
+            setTimeout( currentAction(`It's a hit! Hits total: ${hitsTotal} Batter now on base and a new batter is up!`), 3000);
             // To display another wind up / pitch 
-            setTimeout( currentAction(`It's a hit! Hits total: ${hitsTotal}`), 3000);
             windAndPitchScript();    
                
             // console.log("It's a hit!", "hits total: ", hitsTotal);
@@ -94,7 +109,8 @@ function gamePlay(){
             }
 
 
-    }else if(batter.currentSwing === "strike"){
+    }
+    else if(batter.currentSwing === "strike"){
          
                     // TESTED OK
             strikesTotal = strikesTotal + 1;
@@ -102,62 +118,67 @@ function gamePlay(){
                 // setTimeout(currentAction(`Pitching team wins!`), 3000);
                 // console.log("Pitching team wins!")
                 // console.log("strikes total: ", strikesTotal);
-                setTimeout( endOfGameParade(`Pitching team wins!`), 3000  );
-                return setTimeout( gameOver(), 5000 );
-            }else{
-            setTimeout(currentAction(`Strike! Strikes total: ${strikesTotal}`), 3000);
-            // console.log("Strike!", "strikes total: ", strikesTotal);
+                endOfGameParade(`Three strike outs! Pitching team wins! GAME OVER THX 4 PLAYING`);
+                // Remove swing button 
+                turnOffSwing();
+            }else if(strikesTotal === 3 || strikesTotal === 6){
+
+              // To display current game result AND new batter up 
+              setTimeout(currentAction(`You've struck out! Next batter is up.`), 3000);
+              // To display another wind up / pitch
               windAndPitchScript();                  
+            }else{
+            setTimeout( currentAction(`Strike!`), 3000 );
+              windAndPitchScript();
             }
         
         
-    }else if(batter.currentSwing === "foul"){
+    }
+    else if(batter.currentSwing === "foul"){
 
                     // TESTED OK
             foulsTotal = foulsTotal + 1;
             if(foulsTotal <= 2){
                 strikesTotal = strikesTotal + 1;
+                // To display current game result 
                 setTimeout(currentAction(`Foul ball! Add strike. Fouls total: ${foulsTotal}. Strikes total: ${strikesTotal}`), 3000);
-                // console.log("Foul! Add strike!", "fouls total: ", foulsTotal);
+                // To display another wind up / pitch
                 windAndPitchScript();
             }else{
               setTimeout( currentAction(`WOW U KEEP FOULING OUT lol. Fouls total: ${foulsTotal}`), 3000 );
-              // console.log("WOW U KEEP FOULING OUT", "fouls total: ", foulsTotal);
+                // To display another wind up / pitch
                 windAndPitchScript();
             }
        
 
-    }else if(batter.currentSwing === "ball"){
+    }
+    else if(batter.currentSwing === "ball"){
 
                     // TESTED OK
         ballsTotal = ballsTotal + 1;
-        if(ballsTotal === 4){
+        if(ballsTotal === 4 || ballsTotal === 8 || ballsTotal === 12){
             hitsTotal = hitsTotal + 1;
+
                 if(hitsTotal === 4){
                     // setTimeout( currentAction(`Batting team wins!`), 3000 );           
                     // console.log("Batting team wins");
                     // console.log("hits total: ", hitsTotal);
-                    setTimeout( endOfGameParade(`Batting team wins!`), 3000  );
-                    return setTimeout( gameOver(), 5000 );
+                    // console.log("this ball thing on? ");
+                    endOfGameParade(`Walk pushes a run in! Batting team wins! GAME OVER THX 4 PLAYING`);
+                    // Remove swing button 
+                    turnOffSwing();
                 }else{
-                    setTimeout( currentAction(`Ball! Ball total: ${ballsTotal}`), 3000 );
-                    // console.log("Ball!", "Ball total: ", ballsTotal);
-                     windAndPitchScript();
+                    // To display current game result AND new batter up 
+                    setTimeout( currentAction(`Ball 4! Batter takes base. New batter up! Ball total: ${ballsTotal}`), 3000 );
+                    // To display another wind up / pitch
+                    windAndPitchScript();
                 }
         }else{
         setTimeout( currentAction(`Ball! Ball total: ${ballsTotal}`), 3000 );
-        // console.log("Ball!", "Ball total: ", ballsTotal);
+        // To display another wind up / pitch
         windAndPitchScript();
         }
         
     }
     
 };
-
-
-// gamePlay();
-
-
-// for(let i = 0; i < 10; i++){
-//     gamePlay();
-// }; 
